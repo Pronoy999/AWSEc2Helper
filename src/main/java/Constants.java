d9b1f90d40b01cfce7c811cdfb4de8f8d27528ae
Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>
2019-02-20 11:12:24 +0530

    Update Constants.java

--- src/main/java/Constants.java
+++ src/main/java/Constants.java
@@ -1,4 +1,4 @@
 public class Constants {
-    public static final String ACCESS_KEY_ID = "AKIAJIZWJEI3NUHXOC3Q";
-    public static final String SECRET_ACCESS_KEY = "AY/bIJEgsJ9e4QbjDbVDz1KcyCzffTDgcuPA/nFC";
+    public static final String ACCESS_KEY_ID = "YOUR_ACCESS_KEY";
+    public static final String SECRET_ACCESS_KEY = "YOUR_SECRET_KEY";
 }
